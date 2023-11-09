b0967a4e6998c1aba4788d9752aab6bb688a54b1
FilbertKimasa <filbertdan67@gmail.com>
2023-11-09 07:50:49 +0300

    Add transaction

--- queries.sql
+++ queries.sql
@@ -15,3 +15,21 @@ SELECT * FROM animals WHERE neutered =  true;
 SELECT * FROM animals WHERE name <> 'Gabumon';
 
 SELECT * FROM animals WHERE weight_kg BETWEEN  10.4 and 17.3;
+
+-- transaction start
+
+BEGIN;
+
+DELETE FROM animals WHERE date_of_birth > '2022, 01, 01';
+
+SAVEPOINT save_1;
+
+UPDATE animals SET weight_kg = weight_kg*-1;
+
+ROLLBACK TO save_1;
+
+UPDATE animals SET weight_kg = weight_kg*-1 WHERE weight_kg < 0;
+
+COMMIT;
+
+-- transaction end
