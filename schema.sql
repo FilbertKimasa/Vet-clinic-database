23b37e80ebad6407cb86778d51c6b3e9ded7d2f0
FilbertKimasa <filbertdan67@gmail.com>
2023-11-10 17:26:08 +0300

    Create animals and vets join

--- schema.sql
+++ schema.sql
@@ -42,7 +42,14 @@ CREATE TABLE vets (
 );
 
 CREATE TABLE specializations (
-    vet_id INTEGER REFERENCES vets(id),
-    species_id INTEGER REFERENCES species(id),
+    vet_id INT REFERENCES vets(id),
+    species_id INT REFERENCES species(id),
     PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits (
+    animal_id INT REFERENCES animals(id),
+    vet_id INT REFERENCES vets(id),
+    visit_date DATE,
+    PRIMARY KEY (animal_id, vet_id, visit_date)
 );
